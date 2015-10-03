717e0549b7bb8dfe570fc28a0eadba9b2ebe4b95
sicopa <systems_mj@yahoo.com>
2015-10-03 16:52:25 -0600

    commit 2
    commit 2

--- plantilla.php
+++ plantilla.php
@@ -5,6 +5,7 @@
 <title>Documento sin título</title>
 <!-- Importe de librerias Bootstrap -->
 <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="css/bootstrap-theme.min.css" type="text/css" rel="stylesheet">
 <link href="css/main.css" type="text/css" rel="stylesheet">
 
