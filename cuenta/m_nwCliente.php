e56762df9e1cf946a5a77bc5227dc378492193bf
sicopa <systems_mj@yahoo.com>
2015-10-27 20:45:01 -0600

    Commits areglos

--- cuenta/m_nwCliente.php
+++ cuenta/m_nwCliente.php
@@ -6,7 +6,7 @@
 		
 		
 		$dui = TRIM($_POST["dui"]);
-		$duii= str_replace("-","",$dui);
+		$duis= str_replace("-","",$dui);
 		$nombre = $_POST["nombre"];
 		$apellido = $_POST["apellido"];
 		$nit = TRIM($_POST["nit"]);
@@ -16,15 +16,14 @@
 		$telefono = $_POST["telefono"];
 		$fechanacimiento = $_POST["fechanacimiento"];
 		$profesion = $_POST["cboprofesion"];
-					?><script>alert('<?php echo $profesion; ?>');</script><?php
 		$municipio = $_POST["cbomuni"];
 		$firma = $_POST["cbofirma"];
 		$usuario= $_POST["usuario"];
 		
 		
-		$consultadui=$conn->query("Select CLIENTE_ID from cliente WHERE CLIENTE_ID='$duii' ");
-		$consultanit=$conn->query("Select CLIENTE_NIT from cliente WHERE CLIENTE_NIT='$nitt' ");
+		$consultadui=$conn->query("SELECT CLIENTE_ID FROM cliente WHERE CLIENTE_ID=$duis ");
 
+		
 		
 		
 
@@ -32,11 +31,10 @@
 			header("location: v_nwCliente.php?duplicado=dui");
 		}
 
-		elseif ($consultanit > 0) {
-			header("location: v_nwCliente.php?duplicado=nit");
-		}
+
+		
 		else{
-		$query=$conn->query("INSERT INTO cliente(CLIENTE_ID,MUNICIPIO_ID,PROFESIONES_ID,USER_NICK,CLIENTE_NIT,CLIENTE_NOMBRE,CLIENTE_APELLIDO,CLIENTE_EDAD,CLIENTE_DOMICILIO,CLIENTE_TELEFONO,CLIENTE_FECHANAC,CLIENTE_FIRMA) VALUES('$duii','$municipio','$profesion','$usuario','$nitt','$nombre','$apellido','$edad','$domicilio','$telefono','$fechanacimiento','$firma')");
+		$query=$conn->query("INSERT INTO cliente(CLIENTE_ID,MUNICIPIO_ID,PROFESIONES_ID,USER_NICK,CLIENTE_NIT,CLIENTE_NOMBRE,CLIENTE_APELLIDO,CLIENTE_EDAD,CLIENTE_DOMICILIO,CLIENTE_TELEFONO,CLIENTE_FECHANAC,CLIENTE_FIRMA) VALUES('$duis','$municipio','$profesion','$usuario','$nitt','$nombre','$apellido','$edad','$domicilio','$telefono','$fechanacimiento','$firma')");
 
 		
 		if($query > 0)
