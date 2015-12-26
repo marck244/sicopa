42f47ef7195c49f390924c811689d3d040795fcc
sicopa <systems_mj@yahoo.com>
2015-12-26 11:04:12 -0600

    areglos

--- cuenta/cbomuni.php
+++ cuenta/cbomuni.php
@@ -1,7 +1,13 @@
 <?php
 include('../conexion/conexion.php');
 $salida="";
+$nada="";
 $id_pais=$_POST["elegido"];
+
+if ($id_pais == "0") {
+	$nada.= "<option value=''>Seleccione</option>";
+	echo $nada;
+}
 // construimos el combo de ciudades deacuerdo al pais seleccionado
 $combog = $conn->query("SELECT MUNICIPIO_ID,DEPARTAMENTO_ID,MUNICIPIO_NOMBRE FROM municipio WHERE DEPARTAMENTO_ID='$id_pais' ORDER BY MUNICIPIO_NOMBRE ASC");
   while($sql_p = $combog->fetch_assoc())
